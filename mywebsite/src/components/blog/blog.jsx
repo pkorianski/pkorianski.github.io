2a5b292df36c2e937272db3d215e16cc954e769e
Patrick Korianski <pkorianski@Patricks-MacBook-Pro.local>
2025-08-03 19:05:40 -0400

    testing react website

---
+++ mywebsite/src/components/blog/blog.jsx
@@ -0,0 +1,23 @@
+import '../../App.css';
+
+const Blog = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Page:2 Blog page
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Blog;
